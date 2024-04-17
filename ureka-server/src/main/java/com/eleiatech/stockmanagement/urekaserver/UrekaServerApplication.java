c057aed028f22bdeb8879e6bb1e73f2e3546b2ac
echowang66 <echowangcn@outlook.com>
2024-04-17 13:22:56 +0100

    eureka-server

---
+++ ureka-server/src/main/java/com/eleiatech/stockmanagement/urekaserver/UrekaServerApplication.java
@@ -0,0 +1,16 @@
+package com.eleiatech.stockmanagement.urekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+@SpringBootApplication
+@EnableEurekaServer
+public class UrekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UrekaServerApplication.class, args);
+    }
+
+}
---
+++ ureka-server/src/test/java/com/eleiatech/stockmanagement/urekaserver/UrekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eleiatech.stockmanagement.urekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
